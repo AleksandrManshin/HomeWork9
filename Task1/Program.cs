3863d822df893568331ae8831a90f02069ea316d
Aleksandr <maivan007@yandex.ru>
2022-07-27 18:13:14 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N, кратные трём.
+
+void Zadacha64()
+{
+    Console.Write("Введите первое число промежутка: ");
+    int numbM = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число промежутка: ");
+    int numbN = Convert.ToInt32(Console.ReadLine());
+
+    Recursion(numbM, numbN);
+
+    void Recursion(int numbM, int numbN, int result = 0)
+    {
+        if(numbM > numbN) return;
+
+        if(numbM % 3 == 0)
+        {
+            Console.Write(numbM + " ");
+        }
+        numbM++;
+
+        Recursion(numbM, numbN, result);
+    }
+
+}
+
+Zadacha64();
